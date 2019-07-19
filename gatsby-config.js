804a5bb5708aa827458e4de21dee1dfc40fef8e7
Julian Phillips <julian.p@me.com>
2019-07-18 22:04:42 -0400

    updated siteMetadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title      : `Stacking Your Deck`,
+    description: `We are here to help you level up IRL through the use of technology, nerd-culture, philosophies, and intentionality so that you can win in the game of life.`,
+    author     : `@kadus90`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
+        name            : `gatsby-starter-default`,
+        short_name      : `starter`,
+        start_url       : `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        theme_color     : `#663399`,
+        display         : `minimal-ui`,
+        icon            : `src/images/gatsby-icon.png`,   // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
