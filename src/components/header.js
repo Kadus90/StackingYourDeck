3ecde41fee0c651e3ea67283ce1f6828588dc1dc
Julian Phillips <julian.p@me.com>
2019-07-24 21:47:22 -0400

    Another attempt

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,22 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background  : `#9BD3AE`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin  : `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding : `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
-          to="/"
-          style={{
-            color: `white`,
+          to    = "/"
+          style = {{
+            color         : `white`,
             textDecoration: `none`,
           }}
         >
