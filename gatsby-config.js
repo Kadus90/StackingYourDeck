6b18bb4ac7d3619f0d9d1a99e501b71492ad4efc
Julian Phillips <julian.p@me.com>
2019-08-26 16:59:04 -0400

    Updates to gatsby-config.js, package-lock.json, and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,44 @@
 module.exports = {
-  siteMetadata: {
-    title      : `Stacking Your Deck`,
-    description: `We are here to help you level up IRL through the use of technology, nerd-culture, philosophies, and intentionality so that you can win in the game of life.`,
-    author     : `@kadus90`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name            : `gatsby-starter-default`,
-        short_name      : `starter`,
-        start_url       : `/`,
-        background_color: `#9BD3AE`,
-        theme_color     : `#AAE0FA`,
-        display         : `minimal-ui`,
-        icon            : `src/images/gatsby-icon.png`,   // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+	siteMetadata: {
+		title: `Stacking Your Deck`,
+		description: `We are here to help you level up IRL through the use of technology, nerd-culture, philosophies, and intentionality so that you can win in the game of life.`,
+		author: `@kadus90`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#9BD3AE`,
+				theme_color: `#AAE0FA`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: `gatsby-source-wordpress`,
+			options: {
+				// your wordpress source
+				baseUrl: `uploadvr.com`,
+				protocol: `https`,
+				hostingWPCOM: false,
+				useACF: false,
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 }
